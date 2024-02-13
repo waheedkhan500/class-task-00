0d675293ac50fefb82873e1e7d10b7d222ab797b
waheedkhan500 <waheedxahid500@gmail.com>
2024-02-14 01:24:09 +0500

    I have done with all the 5 tasks

---
+++ tasks.js
@@ -0,0 +1,89 @@
+
+// // //     ***************** Class task 1 **************
+
+// let userInput = prompt("Enter a number here")
+// // let userInput = 23;
+
+// // let number = userInput.toString();
+// // console.log(number)
+
+// let sum = 0;
+// for (let i = userInput.length - 1; i >= 0; i--){
+
+//     sum = Number(userInput[i]) + sum;
+// }
+// console.log(sum)
+
+
+// // //     ***************** Class task 2 **************
+
+// const num1 = parseInt(prompt("Enter the first number of range"))
+// const num2 = parseInt(prompt("Enter the second number of range"))
+// // const arrayOfGivenNumbers = [num1, num2]
+
+// let sum = 0;
+// for (let i = num1; i <= num2; i++){
+//     sum = i + sum; 
+// }
+// console.log(sum)
+
+
+
+// // //     ***************** Class task 3 **************
+
+// let arrayOfNumbers = [5, 6, 8, 10, 15];
+
+// // console.log(arrayOfNumbers.length);
+
+// let sum = 0;
+
+// for (let i = 0; i <= arrayOfNumbers.length - 1; i++){
+
+//     sum = arrayOfNumbers[i] + sum;
+// }
+// // console.log(sum)
+
+// let average = sum / arrayOfNumbers.length;
+// console.log(average);
+
+
+// //     ***************** Class task 4 **************
+
+// let userInput_1 = prompt("ENter string 1 here")
+// let userInput_2 = prompt("ENter string 2 here")
+
+// if (userInput_1.length === userInput_2.length) {
+//     console.log("Yes the length is equal of these two strings");
+// }
+// else {
+//     console.log("No the length is not equal of these two strings")
+// }
+
+
+
+
+
+// //     ***************** Class task 5 **************
+
+// let userInput = prompt("Enter a string here ")
+// let userInputArray = [];
+
+//     userInputArray = userInput.split('');
+
+// //  console.log(userInputArray);
+
+// var firstChar = userInputArray.at(0);
+// // console.log(firstChar)
+
+// var lastChar = userInputArray.at([userInputArray.length-1])
+// // console.log(lastChar);
+
+// userInputArray.splice(0, 1, lastChar);
+
+// userInputArray.splice(userInputArray.length - 1, 1, firstChar)
+// // console.log(userInputArray);
+
+// userInput = userInputArray.join("")
+// console.log(userInput);
+
+
