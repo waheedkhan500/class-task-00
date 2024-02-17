fa56fc558792c6c60500301d473462cd770d7327
waheedkhan500 <waheedxahid500@gmail.com>
2024-02-17 22:04:54 +0500

    Done Quote generator

--- class-task-01/class-task-01.js
+++ class-task-01/class-task-01.js
@@ -1,9 +1,24 @@
 
 // ***************** Welcome Message ******************
 
-function displayWelcomeMessage() {
-    let userName = prompt("Enter your name here:")
-    userName = userName.charAt(0).toUpperCase() + userName.slice(1);
-    alert("Welcome Mr: "+ userName)
-}
-displayWelcomeMessage()
+// function displayWelcomeMessage() {
+//     let userName = prompt("Enter your name here:")
+//     userName = userName.charAt(0).toUpperCase() + userName.slice(1);
+//     alert("Welcome Mr: "+ userName)
+// }
+// displayWelcomeMessage()
+
+
+// ***************** Quote Generator:  ******************
+
+// function generateQuote() {
+//     const quotesArray = ["quote 1","quote 2","quote 3","quote 4","quote 5","quote 6"]
+//     const randomQuote = Math.floor( Math.random() * quotesArray.length)
+
+//     alert("The random quote generated is \n \n"  + quotesArray[randomQuote]);
+
+// }
+// generateQuote();
+
+
+
