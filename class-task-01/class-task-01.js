067c33897f3ddd4bad2d62523559c80f50b02714
waheedkhan500 <waheedxahid500@gmail.com>
2024-02-17 22:21:48 +0500

    Done with random number generator

--- class-task-01/class-task-01.js
+++ class-task-01/class-task-01.js
@@ -21,4 +21,10 @@
 // generateQuote();
 
 
+// ***************** Random Number Generator:  ******************
 
+function randomNumberGenerator() {
+    const randomNumber = Math.ceil(Math.random() * 100)
+    alert("The random number between 1 and 100 is: \n \n" + randomNumber )
+}
+randomNumberGenerator();
